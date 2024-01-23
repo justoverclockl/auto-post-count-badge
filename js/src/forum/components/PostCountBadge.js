17e8e49891fa982cd0ac870df500ebd7e129be61
Wang Xiaojian <74416034+WhXcjm@users.noreply.github.com>
2024-01-23 16:12:42 +0800

    add file function

--- js/src/forum/components/PostCountBadge.js
+++ js/src/forum/components/PostCountBadge.js
@@ -7,6 +7,18 @@ export default class PostCountBadge extends Component {
     const userPosts = this.attrs.posts;
     const userClass = this.attrs.userClass;
     const userBadgeLabel = this.attrs.label;
+    
+    // 检测 userClass 是否以 'fa' 开头
+    const isFontAwesome = /^fa/.test(userClass);
+    // 检测 userClass 是否以 'file:' 开头
+    const isFile = /^file:/.test(userClass);
+
+    // 根据 userClass 的值选择使用 <i> 或 <img>
+    const badgeIcon = isFontAwesome ? (
+      <i class={userClass + ' autopost'} />
+    ) : isFile ? (
+      <img class='autopost' src={userClass.replace('file:', '')} width="26" height="26" alt={userBadgeLabel} />
+    ) : <i class='fas fa-xmark autopost' />;
     return (
       <span>
         <Tooltip
@@ -15,7 +27,7 @@ export default class PostCountBadge extends Component {
           })}
         >
           <span className="auto-badge">
-            <i class={userClass + ' autopost'} />
+            {badgeIcon}
             {userBadgeLabel}
           </span>
         </Tooltip>
